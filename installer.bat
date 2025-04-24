9e8661eba7b42161426c9ca22229cddc7dfadabf
Dori067 <76527614+Dori067@users.noreply.github.com>
2025-04-24 21:53:59 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,49 +1,61 @@
 @echo off
 setlocal
 
-set "download_url=https://cdn.modrinth.com/data/AANobbMI/versions/OwLQelEI/sodium-fabric-0.5.11%2Bmc1.20.6.jar"
-set "download_url2=https://cdn.modrinth.com/data/gvQqBUqZ/versions/5a3sPIH2/lithium-fabric-mc1.20.6-0.12.5.jar"
-set "download_url3=https://cdn.modrinth.com/data/uXXizFIs/versions/i9RcCdZv/ferritecore-6.1.1-fabric.jar"
-set "download_url4=https://cdn.modrinth.com/data/nmDcB62a/versions/dUdr6D7I/modernfix-fabric-5.18.0%2Bmc1.20.6.jar"
+set "download_url_sodium=https://cdn.modrinth.com/data/AANobbMI/versions/OwLQelEI/sodium-fabric-0.5.11+mc1.20.6.jar"
+set "download_url_lithium=https://cdn.modrinth.com/data/gvQqBUqZ/versions/5a3sPIH2/lithium-fabric-mc1.20.6-0.12.5.jar"
+set "download_url_ferritecore=https://cdn.modrinth.com/data/uXXizFIs/versions/i9RcCdZv/ferritecore-6.1.1-fabric.jar"
+set "download_url_modernfix=https://cdn.modrinth.com/data/nmDcB62a/versions/dUdr6D7I/modernfix-fabric-5.18.0+mc1.20.6.jar"
 set "appdata_path=%appdata%"
 set "mods_folder=%appdata_path%\.minecraft\mods"
-set "mod_name=sodium-fabric-0.5.11+mc1.20.6.jar"
-set "mod_name2=lithium-fabric-mc1.20.6-0.12.5.jar"
-set "mod_name3=ferritecore-6.1.1-fabric.jar"
-set "mod_name4=modernfix-fabric-5.18.0+mc1.20.6.jar"
+set "mod_name_sodium=sodium-fabric-0.5.11+mc1.20.6.jar"
+set "mod_name_lithium=lithium-fabric-mc1.20.6-0.12.5.jar"
+set "mod_name_ferritecore=ferritecore-6.1.1-fabric.jar"
+set "mod_name_modernfix=modernfix-fabric-5.18.0+mc1.20.6.jar"
 
-
+echo Creating mods folder if it doesn't exist...
 if not exist "%mods_folder%" (
     mkdir "%mods_folder%"
     if errorlevel 1 (
-        echo Nem sikerult a mods mappat megcsinalni...
+        echo Failed to create the mods folder...
         exit /b 1
     )
 )
 
+echo Downloading Sodium...
+curl -L -o "%mods_folder%\%mod_name_sodium%" "%download_url_sodium%"
+if errorlevel 1 (
+    echo Failed to download Sodium. Please check your internet connection.
+    goto download_failed
+)
 
-echo Letoltes 1....
-curl -L -o "%mods_folder%\%mod_name%" "%download_url%"
+echo Downloading Lithium...
+curl -L -o "%mods_folder%\%mod_name_lithium%" "%download_url_lithium%"
+if errorlevel 1 (
+    echo Failed to download Lithium. Please check your internet connection.
+    goto download_failed
+)
 
+echo Downloading FerriteCore...
+curl -L -o "%mods_folder%\%mod_name_ferritecore%" "%download_url_ferritecore%"
+if errorlevel 1 (
+    echo Failed to download FerriteCore. Please check your internet connection.
+    goto download_failed
+)
 
-echo Letoltes 2....
-curl -L -o "%mods_folder%\%mod_name2%" "%download_url2%"
+echo Downloading ModernFix...
+curl -L -o "%mods_folder%\%mod_name_modernfix%" "%download_url_modernfix%"
+if errorlevel 1 (
+    echo Failed to download ModernFix. Please check your internet connection.
+    goto download_failed
+)
 
-
-echo Letoltes 3....
-curl -L -o "%mods_folder%\%mod_name3%" "%download_url3%"
-
-
-echo Letoltes 4....
-curl -L -o "%mods_folder%\%mod_name4%" "%download_url4%"
-if %errorlevel% equ 0 goto download_success
-
-
-echo Sikertelen volt a fajl letoltese...
-pause
-exit /b 1
-
-:download_success
-echo Sikeresen le vannak toltve a fajlok!!
+echo.
+echo All files downloaded successfully!!
 pause
 exit /b 0
+
+:download_failed
+echo.
+echo One or more files failed to download. Please check the error messages.
+pause
+exit /b 1
